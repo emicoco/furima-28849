d766988a9eded8247127f42434e13e50902acfff
emicoco <coco0418_0325@yahoo.co.jp>
2020-09-07 11:05:22 +0900

    再度コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "items#index"
+
+  # ログアウト
+  devise_scope :social_account do
+    get 'sign_out', to: "sessions#destroy"
+  end
+
+  # 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
