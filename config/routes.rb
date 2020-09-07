3e8c8daad160cea1e101e3df11b1c0d656bc92f1
emicoco <coco0418_0325@yahoo.co.jp>
2020-09-07 20:25:26 +0900

    ユーザ管理機能の修正3

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  # ログアウト
-  devise_scope :social_account do
-    get 'sign_out', to: "sessions#destroy"
-  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
