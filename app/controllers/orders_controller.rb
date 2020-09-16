39921797955229deb5ff01ca94e37346a61b1413
emicoco <coco0418_0325@yahoo.co.jp>
2020-09-16 20:31:05 +0900

    購入画面の遷移とitem表示まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,37 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+  end
+ 
+  def create
+    @order = Order.new(price: order_params[:price])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+ 
+  private
+ 
+  def item_params
+    params.require(:item).permit(:category_id, :image, :name, :description, :price, :status_id, :shipping_method_id, :shipping_origin_area_id, :shipping_days_id).merge(user_id: current_user.id)
   end
 
-  def create
+  def order_params
+    params.permit(:price, :token)
   end
-  
-end
+ 
+  def pay_item
+    Payjp.api_key = "sk_test_XXXXXXXX"
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
+ 
+ end
