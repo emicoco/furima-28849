c6643f6e52d314a02beea4be19559eb4f99ab013
emicoco <coco0418_0325@yahoo.co.jp>
2020-09-16 16:01:57 +0900

    orderコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items
 
+  root to: 'orders#index'
+  resources :orders, only:[:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
