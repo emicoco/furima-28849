abe40306429637f9da46757942a30f68c03b6b52
emicoco <coco0418_0325@yahoo.co.jp>
2020-09-21 15:56:27 +0900

    プレビュー表示機能実装

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,26 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('item-list');
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
+
+      // 画像を表示するためのimg要素を生成
+      const blobImage = document.createElement('img');
+       // 表示する画像を生成
+      blobImage.setAttribute('src', blob);
+
+      // ImageListの中身の確認
+      // console.log(ImageList);   
+
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+      console.log(blob);
+      console.log(blobImage);
+    });
+  });
+}
